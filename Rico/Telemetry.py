bb7e407b6f271c844d173d9c7cd0f6c929396ea5
Rico-Rey94 <rico.rey94@outlook.com>
2025-10-21 15:21:05 -0500

    Update Telemetry.py

--- Rico/Telemetry.py
+++ Rico/Telemetry.py
@@ -4,10 +4,16 @@ app = Flask(__name__)
 
 @app.route('/telemetry', methods=['POST'])
 def telemetry():
-    data = request.get_json()
+    data = request.get_json({
+  "altitude": [Extracted Altitude],
+  "battery": [Battery Level],
+  "lat": [Location Latitude],
+  "lon": [Location Longitude]
+})
     print("📡 Telemetry Received:")
     print(data)
     return jsonify({"status": "ok"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
